558022b943feb4e8fb0636f7d3a5ad0c8951bfba
Alexandre Gois <agois@cpqd.com.br>
2013-06-20 08:53:11 -0300

    Matrix rotation code

--- src/com/agois/javatest/Main.java
+++ src/com/agois/javatest/Main.java
@@ -80,6 +80,18 @@ public class Main {
         }
     }
 
+    static int[][] rotateCW(int[][] mat) {
+        final int M = mat.length;
+        final int N = mat[0].length;
+        int[][] ret = new int[N][M];
+        for (int r = 0; r < M; r++) {
+            for (int c = 0; c < N; c++) {
+                ret[c][M-1-r] = mat[r][c];
+            }
+        }
+        return ret;
+    }
+    
     private static void findTripletInOrderofPositionAndValueOnePass(int[] arr) {
         if (arr.length < 3) return;
 /*        int[] min = new int[arr.length];
